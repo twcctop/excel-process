f0c9bac99076abcc953a786437459bfe3e8f026b
dazuiTian <30425369+chaochao214@users.noreply.github.com>
2021-01-22 21:23:02 +0800

    1

---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+// var chaomao=[1,2,3,4,5]
+// chaomao.splice(2,1,8,9)
+// console.log(chaomao)
+var  jArr=[]
+var  json={}
+json[1]= 'value'; 
+json.text2= 'value2'; 
+ jArr[1]=json
+ jArr[2]=json
+ jArr[3]=json
+ jArr[4]=json
+console.log(json)
+console.log(jArr)
--- src/components/test.vue
+++ src/components/test.vue
@@ -96,6 +96,7 @@ export default {
             resArr[count] = json;
           }
         }
+
         console.log("resArr");
         console.log(resArr);
         /**
